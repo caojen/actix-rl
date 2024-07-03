5ad8ed258fa84afe78167bdff3cf59128f4e018b
caojen <caojen@mail2.sysu.edu.cn>
2024-07-03 17:05:07 +0800

    feat: update

--- examples/mem-cache-middleware.rs
+++ examples/mem-cache-middleware.rs
@@ -9,30 +9,30 @@ use actix_rl::middleware::RateLimit;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Let's create our middleware here
-
-    let rate_limiter = {
-
-        // first, create a store, using store::MemStore.
-        // we initialize the capacity of our mem-store is 1024. The capacity would grow automatically.
-        // we set the expiry duration is 1 minute here.
-        let store = store::MemStore::new(1024, chrono::Duration::minutes(1));
-
-        // then, create a default controller which controls request handling.
-        let controller = controller::Controller::default();
-
-        // finally, create a middleware and return
-        let middleware = RateLimit::new(
-            store,
-            100, // max request count is 100 (per minute).
-            controller,
-        );
-
-        middleware
-    };
-
     // actix-web simple http-server starts here...
     HttpServer::new(|| {
+
+        // Let's create our middleware here
+        let rate_limiter = {
+
+            // first, create a store, using store::MemStore.
+            // we initialize the capacity of our mem-store is 1024. The capacity would grow automatically.
+            // we set the expiry duration is 1 minute here.
+            let store = store::MemStore::new(1024, chrono::Duration::minutes(1));
+
+            // then, create a default controller which controls request handling.
+            let controller = controller::Controller::default();
+
+            // finally, create a middleware and return
+            let middleware = RateLimit::new(
+                store,
+                100, // max request count is 100 (per minute).
+                controller,
+            );
+
+            middleware
+        };
+
         App::new()
             .wrap(rate_limiter)
             .service(web::resource("/hello").route(web::get().to(hello_world)))
